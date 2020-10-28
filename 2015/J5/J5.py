95eedd9997cbb80d05a8c20f8009d579edcd4863
vishnupsatish <vishnupavan.satish@gmail.com>
2020-10-28 18:19:21 -0400

    Add 2015 J5

--- 2015/J5/J5.py
+++ 2015/J5/J5.py
@@ -1,9 +1,21 @@
-pieces = int(input())
-people = int(input())
+n = int(input())
+k = int(input())
 
 ans = 0
 
-def pi(pieces, people, )
+# memo = [[[0 for _ in range(n//k)] for __ in range(n)] for ___ in range(k)]
+
+# print(memo)
 
 
+def pi(pieces, people, min):
+	global ans
+	if people == 1:
+		ans += 1
+		return
+	for i in range(min, pieces//people + 1):
+		pi(pieces - i, people - 1, i)
 
+
+pi(n, k, 1)
+print(ans)
